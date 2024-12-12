1ccfb87334648e3daab758ef59e7ecc371b667d5
efalcon <exzequielfalcon@gmail.com>
2024-12-12 00:29:38 -0300

    Starting portfolio app with Astro

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,6 +1,6 @@
 export const mainMenu = [
      {link:"#", content:"Home", icon:"lucide:home"},
-     {link:"#about", content:"About", icon:"octicon:person-24"},
+     {link:"#about", content:"About me", icon:"octicon:person-24"},
      {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
      {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
